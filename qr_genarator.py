c1c38ab01a4920beead955c945348247865b55bd
Raghav Upadhyay <ru7440@srmist.edu.in>
2023-05-07 11:23:03 +0530

    Add files via upload

---
+++ qr_genarator.py
@@ -0,0 +1,18 @@
+import qrcode
+
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,)
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save = ("qrimage01.png")
+
+
+url = input("Enter site URL to generate: ")
+generate_qrcode(url)
+print("\nYour QRcode is generated")
